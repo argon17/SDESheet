1f9dd256f6d2657b6f0b381a76052a174be0c0aa
argon <premsagark.ug18.ee@nitp.ac.in>
2021-07-19 19:24:37 +0530

    binary search

--- day11 (Binary Search)/SingleElementinaSortedArray.cpp
+++ day11 (Binary Search)/SingleElementinaSortedArray.cpp
@@ -7,6 +7,7 @@ public:
         int lo = 0, hi = nums.size()-1;
         while(lo<hi){
             int mid = (lo+hi)>>1;
+            // mid^1 is too cool, it will just flip the last bit
             if(nums[mid]==nums[mid^1]) lo = mid+1;
             else hi = mid;
         }
