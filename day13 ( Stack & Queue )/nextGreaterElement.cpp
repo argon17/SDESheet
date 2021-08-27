b6d95771353c8c95da8d9e69658ad3c71d43f9c6
argon <premsagark.ug18.ee@nitp.ac.in>
2021-08-27 16:31:49 +0530

    nge problem

--- day13 ( Stack & Queue )/nextGreaterElement.cpp
+++ day13 ( Stack & Queue )/nextGreaterElement.cpp
@@ -1,21 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> nextGreaterElements(vector<int>& nums) {
-    
-    int n = nums.size();
-    stack<int> st;
-    vector<int> ans(n);
-    
-    for(int i=2*n-1; i>=0; --i){
-        
-        while(st.size() && nums[i%n] >= st.top()) st.pop();
-        if(i<n){
-            if(st.empty()) ans[i] = -1;
-            else ans[i%n] = st.top();
+class Solution
+{
+public:
+    vector<int> nextGreaterElements(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 2 * n - 1; i >= 0; --i)
+        {
+            while (st.size() && st.top() <= nums[i % n])
+                st.pop();
+            if (i < n)
+                ans[i] = st.empty() ? -1 : st.top();
+            st.push(nums[i % n]);
         }
-        st.push(nums[i%n]);
-        
+        return ans;
     }
-    return ans;
-}
+};
