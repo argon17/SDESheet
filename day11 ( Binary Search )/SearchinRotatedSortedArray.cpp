1a41916aa9a04eabd29452d49dd92336df8ed12c
argon <premsagark.ug18.ee@nitp.ac.in>
2021-08-29 11:21:51 +0530

    rotated array bs

--- day11 ( Binary Search )/SearchinRotatedSortedArray.cpp
+++ day11 ( Binary Search )/SearchinRotatedSortedArray.cpp
@@ -1,44 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution
 {
 public:
     int search(vector<int> &nums, int target)
     {
         int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l < r)
+        int lo = 0, hi = n - 1;
+        while (lo < hi)
         {
-            int mid = (l + r) >> 1;
-            if (nums[mid] == target) return mid;
-            // check if exists in sorted side
-            // if left half is sorted
-            else if (nums[mid] >= nums[l])
-            {
-                // if exists in left half
-                if (nums[l] <= target && target <= nums[mid]) r = mid;
-                else l = mid + 1;
-            }
-            // if right half is sorted
-            else
-            {
+            int mid = (lo + hi) >> 1;
+            // if right half sorted
+            if(nums[mid] <= nums[hi]) {
                 // if exists in right half
-                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
-                else r = mid;
+                if(nums[mid] < target && target <= nums[hi]) lo = mid + 1;
+                else hi = mid;
+            }
+            // left half sorted
+            else{
+                // if exists in left half
+                if(nums[lo] <= target && target <= nums[mid]) hi = mid;
+                else lo = mid + 1;
             }
         }
-        if(nums[l] == target) return l;
+        if(nums[lo] == target) return lo;
         return -1;
     }
 };
-
-/* Some insights of this format of binary search
-    left half =  [lo ... mid]
-    right half = [(mid+1) ... hi]
-    ignore left half => lo = mid + 1
-    ignore right half => hi = mid
-*/
