aba4f3ee76fe250581e7fcc28adb44d3173d4011
argon <premsagark.ug18.ee@nitp.ac.in>
2021-08-29 09:40:27 +0530

    rotated array bs

--- day11 ( Binary Search )/SearchinRotatedSortedArray.cpp
+++ day11 ( Binary Search )/SearchinRotatedSortedArray.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution
 {
 public:
@@ -8,26 +11,34 @@ public:
     {
         int n = nums.size();
         int l = 0, r = n - 1;
-        while (l <= r)
+        while (l < r)
         {
             int mid = (l + r) >> 1;
             if (nums[mid] == target) return mid;
             // check if exists in sorted side
             // if left half is sorted
-            else if (nums[mid] > nums[r])
+            else if (nums[mid] >= nums[l])
             {
                 // if exists in left half
-                if (target < nums[mid] && target >= nums[l]) r = mid - 1;
+                if (nums[l] <= target && target <= nums[mid]) r = mid;
                 else l = mid + 1;
             }
             // if right half is sorted
             else
             {
                 // if exists in right half
-                if (target > nums[mid] && target <= nums[r]) l = mid + 1;
-                else r = mid - 1;
+                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
+                else r = mid;
             }
         }
+        if(nums[l] == target) return l;
         return -1;
     }
 };
+
+/* Some insights of this format of binary search
+    left half =  [lo ... mid]
+    right half = [(mid+1) ... hi]
+    ignore left half => lo = mid + 1
+    ignore right half => hi = mid
+*/
