4ada7cd13a1f2a9c6327f28bf9bf36483c51556b
argon <premsagark.ug18.ee@nitp.ac.in>
2021-09-18 10:26:59 +0530

    improved code for sort colora

--- day01 ( Arrays )/sort012.cpp
+++ day01 ( Arrays )/sort012.cpp
@@ -6,29 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    void swap(int& a, int& b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
     void sortColors(vector<int>& nums) {
-        if(nums.size()==1) return;
-        int lo = 0, mid = 0, hi = nums.size()-1;
-        //before lo, all are zeros
-        //after hi all are 2
-        //operation starts from mid
-        while(mid<=hi){
-            switch(nums[mid]){
-                case 0: //put it before lo
-                    swap(nums[mid++], nums[lo++]);
-                    break;
-                case 1: //nothing to do
-                    mid++;
-                    break;
-                case 2: //put it after hi
-                    swap(nums[mid], nums[hi--]);
-                    break;
-            }
+        int n = nums.size();
+        // before lo all are 0's
+        // after hi all are 2's
+        int lo = 0, hi = n-1;
+        for(int i=0; i<=hi; ++i){
+            if(nums[i]==2) swap(nums[i--], nums[hi--]);
+            else if(nums[i]==0) swap(nums[i], nums[lo++]);
         }
     }
 };
